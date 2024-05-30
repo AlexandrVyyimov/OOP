562d749df88b6ddaedf9b36960020a043bcc8f9b
Alexandr <vyymov90@yandex.ru>
2024-05-31 00:29:09 +0400

    2 pflfybt

--- stydent.py
+++ stydent.py
@@ -9,7 +9,7 @@ class Student:
 
 
 class Mentor:
-    def __init__(self, name, surname):
+    def __init__(self, name, surname,):
         self.name = name
         self.surname = surname
         self.courses_attached = []
@@ -22,16 +22,25 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
-
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(self, name, surname)
+        self.courses_attached = []
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(self, name, surname)
+        self.courses_attached = []
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
+cool_mentor = Reviewer('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+print(best_student.grades)fghfgh
+fhijpfoihjpfijpgs
+sfjhfgh
